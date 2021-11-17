37bb819cff348e04f4b8e1f02fb90d5a68bf0ec6
rafaelagf12 <94558972+rafaelagf12@users.noreply.github.com>
2021-11-17 14:44:20 -0300

    Update phx_actions.ex

--- lib/phx_actions.ex
+++ lib/phx_actions.ex
@@ -6,4 +6,8 @@ defmodule PhxActions do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  
+  def test123 do
+    IO.inspect("TEST 123")
+  end
 end
