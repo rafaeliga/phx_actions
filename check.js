c8c0902350d81aa0d1c1360d6c3d4f8308948088
Rafael Iga <rafael.f.iga@gmail.com>
2021-11-29 15:07:16 -0300

    require fs

--- check.js
+++ check.js
@@ -1,18 +1,17 @@
-import {readFileSync} from 'fs'
-
 module.exports = async ({github, context}) => {
-  console.log("=================================== CHECK ===================")
-  console.log(process.env)
+  // console.log(process.env)
   
-  await github.request(`POST ${context.payload.pull_request._links.comments.href}`, {
-    body: 'test action comment SCRIPT2',
-    commit_id: context.sha,
-    line: 2,
-    path: "lib/phx_actions/other_new.ex"
-  })
+  // await github.request(`POST ${context.payload.pull_request._links.comments.href}`, {
+  //   body: 'test action comment SCRIPT2',
+  //   commit_id: context.sha,
+  //   line: 2,
+  //   path: "lib/phx_actions/other_new.ex"
+  // })
+
+  const fs = require('fs')
 
   let file = JSON.parse(
-    readFileSync(`${process.env}/files.json`, {encoding: 'utf8'})
+    fs.readFileSync(`${process.env.HOME}/files.json`, {encoding: 'utf8'})
   )
   console.log(file)
 
