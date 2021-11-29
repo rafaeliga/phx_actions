ee9efb306594744c1c59f7638ed2c61a625576fb
Rafael Iga <rafael.f.iga@gmail.com>
2021-11-29 15:28:47 -0300

    check evns

--- check.js
+++ check.js
@@ -1,5 +1,6 @@
 module.exports = async ({github, context}) => {
-  // console.log(process.env)
+  console.log(github)
+  console.log(context)
   
   const fs = require('fs')
 
@@ -7,17 +8,14 @@ module.exports = async ({github, context}) => {
     fs.readFileSync(`${process.env.HOME}/files.json`, {encoding: 'utf8'})
   )
   
-  files.forEach(file => {
-    console.log(file)
-    console.log(context.sha)
-
-    github.request(`POST ${context.payload.pull_request._links.comments.href}`, {
-      body: 'You missed this file to create a test',
-      commit_id: context.sha,
-      line: 1,
-      path: file
-    })
-  });
+  // files.forEach(file => {
+  //   github.request(`POST ${context.payload.pull_request._links.comments.href}`, {
+  //     body: 'You missed this file to create a test',
+  //     commit_id: context.sha,
+  //     line: 1,
+  //     path: file
+  //   })
+  // });
 
   return context.issue.number
 }
