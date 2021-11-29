56ef83c3cc23f64f979b32309590cb46541f111b
Rafael Iga <rafael.f.iga@gmail.com>
2021-11-29 14:46:26 -0300

    reading files

--- check.js
+++ check.js
@@ -1,15 +1,16 @@
 module.exports = async ({github, context}) => {
   console.log("=================================== CHECK ===================")
-  console.log(`=================================== PR: ${context.payload.pull_request.number} ===================`)
-  console.log(context.repo)
-  console.log(context.payload.pull_request)
-
-  await github.request(`POST /repos/${content.repo.repo}/pulls/${context.payload.pull_request.number}/comments`, {
+  console.log(process.env)
+  
+  await github.request(`POST ${context.payload.pull_request._links.comments.href}`, {
     body: 'test action comment SCRIPT',
     commit_id: context.sha,
     line: 2,
     path: "lib/phx_actions/other_new.ex"
   })
+
+  let file = require('files.json')
+  console.log(file)
 
   return context.issue.number
 }
