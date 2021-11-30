dde6f3042cf15479a4daf426339261501dd3cd7d
Rafael Iga <rafael.f.iga@gmail.com>
2021-11-30 11:09:59 -0300

    fix post url

--- check.js
+++ check.js
@@ -1,28 +1,16 @@
 module.exports = async ({github, context}) => {
-  console.log("original:", `/repos/${github.repository}/pulls/15/comments`);
-  let from_payload = context.payload.pull_request.comments_url
-  console.log("FROM: ", from_payload)
-
-  await github.request(`POST ${from_payload}`, {
-    body: 'SINGLE inside',
-    commit_id: context.payload.pull_request.head.sha,
-    line: 2,
-    path: "lib/phx_actions/other_new.ex"
-  })
-
   const fs = require('fs')
 
   let files = JSON.parse(
     fs.readFileSync(`${process.env.HOME}/files.json`, {encoding: 'utf8'})
   )
+
+  console.log
   
   files.forEach(file => {
-    console.log(file)
-    console.log(context.payload.pull_request.head.sha)
-
-    github.request(`POST ${context.payload.pull_request.comments_url}`, {
+    github.request(`POST /repos/${process.env.GITHUB_REPOSITORY}/pulls/${context.payload.pull_request.number}/comments`, {
       body: 'You missed this file to create a test',
-      // commit_id: context.payload.pull_request.head.sha,
+      commit_id: context.payload.pull_request.head.sha,
       line: 1,
       path: file
     })
