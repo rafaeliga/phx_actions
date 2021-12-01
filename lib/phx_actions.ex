4ccc7497c6430a2aa40b97eb50f2bb7584d006b6
Rafael Iga <rafael.f.iga@gmail.com>
2021-12-01 14:42:19 -0300

    change reporter

--- lib/phx_actions.ex
+++ lib/phx_actions.ex
@@ -2,16 +2,4 @@ defmodule PhxActions do
   def test_action2 do
     String.codepoints("2") |> IO.inspect()
   end
-
-  def test_action3 do
-    String.codepoints("3") |> IO.inspect()
-  end
-
-  def test_action5 do
-    "a" |> IO.inspect()
-  end
-
-  def test_action5 do
-    IO.inspect "a"
-  end
 end
