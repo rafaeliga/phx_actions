f728f3a870472cea4876a50194f763bd3efe7579
Rafael Iga <rafael.f.iga@gmail.com>
2021-12-01 12:54:55 -0300

    test: tests

--- lib/phx_actions.ex
+++ lib/phx_actions.ex
@@ -1,17 +1,5 @@
 defmodule PhxActions do
-  @moduledoc """
-  PhxActions keeps the contexts that define your domain
-  and business logic.
-
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
-
-  def test_action_new do
-    String.codepoints("xxxx") |> IO.inspect()
-  end
-
-  def test_action_reviewdob do
-    String.codepoints("OK") |> IO.inspect()
+  def test_action do
+    String.codepoints("1") |> IO.inspect()
   end
 end
