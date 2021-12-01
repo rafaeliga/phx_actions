fd9c1ad4b1ee815e96d362847ac992a8f8045ea5
Rafael Iga <rafael.f.iga@gmail.com>
2021-12-01 15:49:45 -0300

    no filter

--- lib/phx_actions.ex
+++ lib/phx_actions.ex
@@ -1,11 +1,14 @@
 defmodule PhxActions do
   def test_action do
-    String.codepoints("1")
+    String.codepoints("2") |> IO.inspect()
   end
 end
 
 defmodule PhxActionsNew do
   def test_action_new do
-    IO.inspect("123")
+    IO.inspect("1")
   end
+end
+
+defmodule PhxActionsNewOne do
 end
