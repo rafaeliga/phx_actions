a7f24853e5271aaed447bb2d5e8997ce3087542e
Rafael Iga <rafael.f.iga@gmail.com>
2021-12-01 16:03:14 -0300

    duplicated annotation

--- lib/phx_actions.ex
+++ lib/phx_actions.ex
@@ -1,14 +1,7 @@
 defmodule PhxActions do
+  @moduledoc false
+
   def test_action do
-    IO.inspect(String.codepoints("2"))
+    "2" |> String.codepoints()
   end
-end
-
-defmodule PhxActionsNew do
-  def test_action_new do
-    IO.inspect("1")
-  end
-end
-
-defmodule PhxActionsNewOne do
 end
