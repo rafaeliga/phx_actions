d1bba90f4179ee3b8c1a427254dff249377d37fa
Rafael Iga <rafael.f.iga@gmail.com>
2021-12-01 14:34:49 -0300

    test group

--- lib/phx_actions.ex
+++ lib/phx_actions.ex
@@ -6,4 +6,12 @@ defmodule PhxActions do
   def test_action3 do
     String.codepoints("3") |> IO.inspect()
   end
+
+  def test_action5 do
+    "a" |> IO.inspect()
+  end
+
+  def test_action5 do
+    IO.inspect "a"
+  end
 end
